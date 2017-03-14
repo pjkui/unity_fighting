078597de906f031d25bb6f68ba81df30a9f0690b
Quinn Pan <pjkui@qq.com>
2017-03-14 09:56:30 +0800

    add project

---
+++ Assets/FightingUnityChan_FreeAsset/FightingUnityChan_FreeAsset/Scripts/IdleChanger.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//
+// アニメーション簡易プレビュースクリプト
+// 2015/4/25 quad arrow
+//
+
+// Require these components when using this script
+[RequireComponent(typeof(Animator))]
+
+public class IdleChanger : MonoBehaviour
+{
+
+    private Animator anim;                      // Animatorへの参照
+    private AnimatorStateInfo currentState;     // 現在のステート状態を保存する参照
+    private AnimatorStateInfo previousState;    // ひとつ前のステート状態を保存する参照
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // 各参照の初期化
+        anim = GetComponent<Animator>();
+        currentState = anim.GetCurrentAnimatorStateInfo(0);
+        previousState = currentState;
+
+    }
+
+
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width - 200, 45, 120, 350), "");
+        if (GUI.Button(new Rect(Screen.width - 190, 60, 100, 20), "Jab"))
+            anim.SetBool("Jab", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 90, 100, 20), "Hikick"))
+            anim.SetBool("Hikick", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 120, 100, 20), "Spinkick"))
+            anim.SetBool("Spinkick", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 150, 100, 20), "Rising_P"))
+            anim.SetBool("Rising_P", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 180, 100, 20), "Headspring"))
+            anim.SetBool("Headspring", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 210, 100, 20), "Land"))
+            anim.SetBool("Land", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 240, 100, 20), "ScrewKick"))
+            anim.SetBool("ScrewK", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 270, 100, 20), "DamageDown"))
+            anim.SetBool("DamageDown", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 300, 100, 20), "SamK"))
+            anim.SetBool("SAMK", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 330, 100, 20), "Run"))
+            anim.SetBool("Run", true);
+        if (GUI.Button(new Rect(Screen.width - 190, 360, 100, 20), "Run_end"))
+            anim.SetBool("Run", false);
+
+    }
+
+    // Update is called every frame, if the MonoBehaviour is enabled
+    public void Update()
+    {
+        var dirEvent = Input.anyKeyDown;
+        if (dirEvent)
+        {
+
+            switch (Input.inputString)
+            {
+                case "j":
+                    {
+                        anim.SetBool("Jab", true);
+                        break;
+                    }
+                case "k":
+                    {
+                        anim.SetBool("Hikick", true);
+                        break;
+                    }
+                case "l":
+                    {
+                        anim.SetBool("Spinkick", true);
+                        break;
+                    }
+                case "u":
+                    {
+                        anim.SetBool("Rising_P", true);
+                        break;
+                    }
+                case "i":
+                    {
+                        anim.SetBool("Headspring", true);
+                        break;
+                    }
+                case "o":
+                    {
+                        anim.SetBool("Run", true);
+                        break;
+                    }
+            }
+        }
+    }
+}
